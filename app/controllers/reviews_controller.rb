55e14cb9a7ac87997d04af43280708fdbb59e121
Susheel Shivaprasad <susheelshivaprasad@gmail.com>
2021-08-18 16:30:29 +0100

    restyled for neater stuff

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,7 +8,7 @@ class ReviewsController < ApplicationController
   def create
     # @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
-     @review.restaurant = @restaurant
+    @review.restaurant = @restaurant
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: [:index, :new,:create, :show] do
+  resources :restaurants, only: %i[index new create show] do
 
 
-    resources :reviews, only: [:index, :new, :create]
+    resources :reviews, only: %i[index new create]
   end
 end
